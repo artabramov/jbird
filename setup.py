8ffdb1cf70105c28573c434bb037d1da2280542b
Artem Abramov <notdepot@gmail.com>
2021-03-29 15:32:55 +0000

    version update

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup
 
 setup(name='jbird',
-      version='0.0.3',
+      version='0.0.4',
       description='Key/value data storage',
       packages=['jbird'],
       author_email='notdepot@gmail.com',
